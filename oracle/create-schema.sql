f739b2c593cdb2b9761ce1fda6d16323b8338ed6
Peter Limpens <peter@wearenorth.eu>
2025-04-16 14:46:06 +0200

    MASA-301 add-environments-to-test-mssql

--- oracle/create-schema.sql
+++
@@ -1,7 +0,0 @@
--- Create user
-CREATE user c##masadb IDENTIFIED BY rootpassword;
-DEFAULT TABLESPACE "USERS"
-TEMPORARY TABLESPACE "TEMP";
-
--- Set user roles
-ALTER USER "masadb" DEFAULT ROLE "DBA";
